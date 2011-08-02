b9b49f54bc93cd16e1e95417ec73290c024052a1
Kyle Meyer <kaiuhl@gmail.com>
2011-08-02 09:59:57 -0700

    Redirect to previous page on login

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -8,11 +8,7 @@ class UserSessionsController < ApplicationController
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
       flash[:notice] = "You are now logged in."
-			if request.env["PATH_INFO"] == "/user_sessions" || request.env["PATH_INFO"] == "/login"
-				redirect_to root_path
-			else
-      	redirect_to :back rescue redirect_to root_path
-			end
+     	redirect_to :back rescue redirect_to root_path
     else
       render :action => 'new', :layout => "small"
     end
