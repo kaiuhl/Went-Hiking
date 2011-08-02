2611bb046172e32950c13ff7f52355a8f3dfcfd1
Kyle Meyer <kaiuhl@gmail.com>
2011-08-02 09:59:43 -0700

    Fix elevation/mile standings

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -5,7 +5,7 @@ class StaticController < ApplicationController
 		@top_mileage = User.limit(10).order("ytd_mileage DESC")
 		@top_elevation = User.where(["ytd_mileage > ? AND ytd_elevation > ?", 0, 0]).order("ytd_elevation DESC").limit(10)
 		@top_nights = User.where(["ytd_nights > ? AND ytd_mileage > ?", 0, 0]).order("ytd_nights DESC").limit(10)
-		@top_elevation_per_mile = User.where(["ytd_mileage >= ?", 100]).limit(10).sort{|a,b| (b.ytd_elevation / b.ytd_mileage) <=> (a.ytd_elevation / a.ytd_mileage) }
+		@top_elevation_per_mile = User.where(["ytd_mileage >= ?", 100]).sort{|a,b| (b.ytd_elevation / b.ytd_mileage) <=> (a.ytd_elevation / a.ytd_mileage) }[0..9]
 		@newest_members = User.limit(5).order("created_at DESC")
 		@top_hikes = Hike.limit(5).where("")		
 		@user_count = User.count
